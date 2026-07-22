db805aa142fc86ff4145066f4eb560951e85f4bb
ggsphp-afk <ggs.php@gmail.com>
2026-07-21 21:08:56 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/exemplo/assistenteuber/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.exemplo.assistenteuber
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
