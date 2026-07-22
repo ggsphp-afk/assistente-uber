8d2395f7204d450b5e49c76ff5368ad4373cc038
ggsphp-afk <ggs.php@gmail.com>
2026-07-21 21:25:03 -0300

    Update MainActivity.kt

--- app/src/main/java/com/exemplo/assistenteuber/MainActivity.kt
+++ app/src/main/java/com/exemplo/assistenteuber/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.exemplo.assistenteuber
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this).apply {
+            text = "Assistente Uber Rodando!"
+            textSize = 20f
+            setPadding(50, 50, 50, 50)
+        }
+        
+        setContentView(textView)
     }
 }
